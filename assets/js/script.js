58c042c69bc1ab3f59ef44e47f860a3dd7515e1f
Madeline Urbanic <madelinerurbanic@gmail.com>
2023-09-16 14:40:28 -0400

    change list item questions to be controlled by variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,32 @@
-var itemOne = document.querySelector(".item-1")
 
+
+var itemDisplayOne = document.querySelector(".item-1")
+var itemDisplayTwo = document.querySelector(".item-2")
+var itemDisplayThree = document.querySelector(".item-3")
+var itemDisplayFour = document.querySelector(".item-4")
+
+var questionOptionOne
+var questionOptionTwo
+var questionOptionThree
+var questionOptionFour
+
+function setQuestion(){
+    questionOptionOne = "One"
+    questionOptionTwo = ":)"
+    questionOptionThree = "Question"
+    questionOptionFour = "Test"
+}
+
+setQuestion();
+
+
+/* Change list items based on question selected */
 function changeItem(){
-    itemOne.textContent = "Change"
+    itemDisplayOne.textContent = questionOptionOne
+    itemDisplayTwo.textContent = questionOptionTwo
+    itemDisplayThree.textContent = questionOptionThree
+    itemDisplayFour.textContent = questionOptionFour
 
 }
 
-changeItem()
+changeItem();
